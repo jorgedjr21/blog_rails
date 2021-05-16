97cd2547313b978086d9cbe212a090a94453d1b0
Jorge Junior <jorgedjr21@gmail.com>
2021-05-16 18:43:15 -0300

    show posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: :show
   def index
     @posts = Post.eager_load(:author).order(created_at: :desc).all.to_a
     @featured = @posts.shift
+  end
+
+  def show; end
+
+  private
+
+
+  def set_post
+    @post = Post.eager_load(:author).find(params[:id])
   end
 end
